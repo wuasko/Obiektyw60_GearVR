35ad7210b89c7cec5309b79418a05cb97fc2a847
Aleksandra Kotula <o.k.nameless@gmail.com>
2019-01-29 00:31:03 +0100

    GearVRPlayer

---
+++ Assets/Scripts/Movement/GearMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearMovement : MonoBehaviour {
+
+
+    public float speed = 1.0f;
+
+    CharacterController controller;
+
+    public GameObject centerEye;
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Define the forward vector using your facing direction
+        //Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 forward = centerEye.transform.forward;
+
+        // If the touchpad is being held down, move forward in the direction you are facing.
+        if (Input.GetMouseButton(0))
+        {
+            controller.SimpleMove(forward * speed);
+        }
+    }
+}
